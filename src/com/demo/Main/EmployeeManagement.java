30b8eeb5920d9322078289cce7f1e69789374a61
cvikesh <cvikesh@ford.com>
2022-09-28 16:12:08 +0530

    second changes

--- src/com/demo/Main/EmployeeManagement.java
+++ src/com/demo/Main/EmployeeManagement.java
@@ -18,7 +18,7 @@ public class EmployeeManagement {
 		
 		EmployeeServiceLayer employeeService = new EmployeeServiceImplementation();
 		
-		
+		System.out.println("Welcome");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter 1 to insert employees");
 		System.out.println("Enter 2 to show employees");
